a5d8ddef952867efb428dbb633fc4cd495b0fbd6
Harry Kainth <kainth247@gmail.com>
2023-06-02 02:59:22 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,25 @@ module.exports = {
       filename: '../database.sqlite3',
     },
     useNullAsDefault: true,
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_URL || process.env.PGDATABASE,
+      host: process.env.PGHOST,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 }
